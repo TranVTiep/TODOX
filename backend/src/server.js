a2ca51913bb7533c51408d8f333478ca48c65fe9
Tran Tiep <conandudo343@gmail.com>
2026-02-03 00:53:49 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,25 @@ import tasksRoutes from "./routes/tasksRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // để lấy đường dẫn tuyệt đối
 const app = express(); // ✅ tạo app trước
 app.use(express.json()); // ✅ middleware parse JSON
-app.use(cors({ origin: "http://localhost:5173" })); // cho phép CORS
+if (process.env.NODE_ENV !== "development") {
+  app.use(cors({ origin: "http://localhost:5173" })); // cho phép CORS
+}
 
 app.use("/api/tasks", tasksRoutes);
 
+// Phục vụ tệp tĩnh từ thư mục frontend/build
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
